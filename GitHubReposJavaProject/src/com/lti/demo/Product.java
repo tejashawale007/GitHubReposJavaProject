4ef20021048c914e562c6be3db931bc2ff46fba4
TejasHavale <tejashawale007@gmail.com>
2021-09-15 15:03:59 +0530

    First Commit

---
+++ GitHubReposJavaProject/src/com/lti/demo/Product.java
@@ -0,0 +1,35 @@
+package com.lti.demo;
+
+public class Product {
+	private int prdId;
+	private String prdName;
+	private double prdCost;
+	public Product(int prdId, String prdName, double prdCost) {
+		super();
+		this.prdId = prdId;
+		this.prdName = prdName;
+		this.prdCost = prdCost;
+	}
+	public int getPrdId() {
+		return prdId;
+	}
+	public void setPrdId(int prdId) {
+		this.prdId = prdId;
+	}
+	public String getPrdName() {
+		return prdName;
+	}
+	public void setPrdName(String prdName) {
+		this.prdName = prdName;
+	}
+	public double getPrdCost() {
+		return prdCost;
+	}
+	public void setPrdCost(double prdCost) {
+		this.prdCost = prdCost;
+	}
+	@Override
+	public String toString() {
+		return "Product [prdId=" + prdId + ", prdName=" + prdName + ", prdCost=" + prdCost + "]";
+	}
+}
---
+++ GitHubReposJavaProject/src/com/lti/demo/UserApp.java
@@ -0,0 +1,9 @@
+package com.lti.demo;
+
+public class UserApp {
+
+	public static void main(String args[]) {
+		Product p=new Product(101,"Mobile",5000);
+		System.out.println(p);
+	}
+}
